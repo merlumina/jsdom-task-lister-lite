b2d46c2cb70485c062f231c20c7b4c3d6ffcd1b2
Liz Wilson <lizriderwilson@gmail.com>
2021-06-17 11:24:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskFormSubmitBtn = document.getElementById("new-task-submit-btn");
+  const taskDescription = document.getElementById("new-task-description");
+  const taskUL = document.getElementById("tasks");
+
+  taskFormSubmitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const newTask = document.createElement("li");
+    newTask.innerText = taskDescription.value;
+    taskUL.append(newTask);
+    
+  });
+
 });
